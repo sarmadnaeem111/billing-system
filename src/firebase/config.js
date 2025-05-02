7e361adcb20b2a97eea129850b9843d86313336b
sarmadnaeem111 <sarmadnaeem111@gmail.com>
2025-05-02 10:18:38 +0500

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,21 @@ import { getStorage } from 'firebase/storage';
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAVTq89H0x_KzlgxiobbCGUhdnqICsBi48",
-  authDomain: "customer-abe40.firebaseapp.com",
-  projectId: "customer-abe40",
-  storageBucket: "customer-abe40.firebasestorage.app",
-  messagingSenderId: "566208631479",
-  appId: "1:566208631479:web:540f9812eceb08690cb332",
-  measurementId: "G-BKJVVKWWV2"
+  // apiKey: "AIzaSyAVTq89H0x_KzlgxiobbCGUhdnqICsBi48",
+  // authDomain: "customer-abe40.firebaseapp.com",
+  // projectId: "customer-abe40",
+  // storageBucket: "customer-abe40.firebasestorage.app",
+  // messagingSenderId: "566208631479",
+  // appId: "1:566208631479:web:540f9812eceb08690cb332",
+  // measurementId: "G-BKJVVKWWV2"
+
+    apiKey: "AIzaSyCAts2_k5koBbKEpEYN7FHAV8kVnPVnuQg",
+  authDomain: "billiing-system.firebaseapp.com",
+  projectId: "billiing-system",
+  storageBucket: "billiing-system.firebasestorage.app",
+  messagingSenderId: "902814918340",
+  appId: "1:902814918340:web:2b4244de8b79a57c9a0b91",
+  measurementId: "G-Q3GRMJXXQ8"
 };
 
 // Initialize Firebase
